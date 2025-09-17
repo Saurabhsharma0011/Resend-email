6cd6964351d6c6ffb98e23b7f6c4b43c7739324e
Saurabh Sharma <146759278+Saurabhsharma0011@users.noreply.github.com>
2025-09-18 01:55:45 +0530

    Update route.ts

--- app/send/api/route.ts
+++ app/send/api/route.ts
@@ -1,23 +1,24 @@
 import { EmailTemplate } from '../../../components/email-template';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function POST() {
+export async function POST(req: Request) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: ['saurabh.sharma9827@gmail.com'],
-      subject: 'Hello world',
+      subject: 'this is the a trail messge where i want to know this is the correct mail',
       react: EmailTemplate({ firstName: 'John' }),
     });
 
     if (error) {
+      console.error({ error });
       return Response.json({ error }, { status: 500 });
     }
 
     return Response.json(data);
   } catch (error) {
+    console.error({ error });
     return Response.json({ error }, { status: 500 });
   }
 }
